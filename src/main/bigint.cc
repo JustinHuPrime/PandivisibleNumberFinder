6a91dac345a139a34a39b39e2a34ba0058151410
Justin Hu <justin.hu@alumni.ubc.ca>
2019-12-12 21:16:25 -0800

    added big ints

---
+++ src/main/bigint.cc
@@ -0,0 +1,30 @@
+// Copyright 2019 Justin Hu
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// This file is part of the Pandivisible Number Finder.
+
+#include "bigint.h"
+
+namespace pandivisible {
+BigInt::BigInt(int _base) noexcept : base(_base), digits() {}
+BigInt::BigInt(int _base, int init) noexcept : base(_base), digits(1, init) {}
+size_t BigInt::size() const noexcept { return digits.size(); }
+
+int operator%(BigInt const &number, int modulus) noexcept {
+  int remainder = 0;
+  for (int digit : number.digits)
+    remainder = (digit + number.base * remainder) % modulus;
+  return remainder;
+}
+}  // namespace pandivisible
---
+++ src/main/bigint.h
@@ -0,0 +1,50 @@
+// Copyright 2019 Justin Hu
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// This file is part of the Pandivisible Number Finder.
+
+#ifndef PANDIVISIBLE_BIGINT_H_
+#define PANDIVISIBLE_BIGINT_H_
+
+#include <cstdint>
+#include <vector>
+
+namespace pandivisible {
+// a big integer with some base
+// assumes no leading zeroes
+class BigInt {
+ public:
+  BigInt(int base) noexcept;
+  BigInt(int base, int digit) noexcept;
+  BigInt(BigInt const &) noexcept = default;
+  BigInt(BigInt &&) noexcept = default;
+
+  ~BigInt() noexcept = default;
+
+  BigInt &operator=(BigInt const &) noexcept = default;
+  BigInt &operator=(BigInt &&) noexcept = default;
+
+  friend int operator%(BigInt const &, int modulus) noexcept;
+
+  size_t size() const noexcept;
+
+ private:
+  int const base;
+  std::vector<int> digits;
+};
+
+int operator%(BigInt const &number, int modulus) noexcept;
+}  // namespace pandivisible
+
+#endif  // PANDIVISIBLE_BIGINT_H_
