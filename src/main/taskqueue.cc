e5efa3900dafc9bf425cd9339585500a7fcfca5e
Justin Hu <justin.hu@alumni.ubc.ca>
2019-12-12 22:32:50 -0800

    added task queue

---
+++ src/main/taskqueue.cc
@@ -0,0 +1,59 @@
+// Copyright 2019 Justin Hu
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// This file is part of the Pandivisible Number Finder.
+
+#include "taskqueue.h"
+
+using namespace std;
+
+namespace pandivisible {
+char const *NoTaskException::what() const noexcept {
+  return "no tasks left in queue";
+}
+TaskQueue::TaskQueue() noexcept
+    : lock(), newTaskFlag(), tasks(), maybeMoreCount(0) {}
+void TaskQueue::push(BigInt &&task) noexcept {
+  lock_guard synchronize(lock);
+  tasks.push(move(task));
+  newTaskFlag.notify_one();
+}
+void TaskQueue::push(BigInt const &task) noexcept {
+  lock_guard synchronize(lock);
+  tasks.push(task);
+  newTaskFlag.notify_one();
+}
+BigInt TaskQueue::pop() {
+  unique_lock synchronize(lock);
+  while (tasks.empty() && maybeMoreCount != 0) {
+    // wait until tasks isn't empty or until maybeMoreCount is zero
+    newTaskFlag.wait(synchronize);
+  }
+
+  if (tasks.empty()) {
+    // no more tasks!
+    throw NoTaskException();
+  } else {
+    BigInt retval = tasks.front();
+    tasks.pop();
+    maybeMoreCount++;  // this task might generate more tasks
+    return retval;
+  }
+}
+void TaskQueue::done() noexcept {
+  lock_guard synchronize(lock);
+  maybeMoreCount--;
+  if (maybeMoreCount == 0) newTaskFlag.notify_all();
+}
+}  // namespace pandivisible
---
+++ src/main/taskqueue.h
@@ -0,0 +1,75 @@
+// Copyright 2019 Justin Hu
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// This file is part of the Pandivisible Number Finder.
+
+#ifndef PANDIVISIBLE_TASKQUEUE_H_
+#define PANDIVISIBLE_TASKQUEUE_H_
+
+#include "bigint.h"
+
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+
+namespace pandivisible {
+// no tasks left exception
+class NoTaskException : public std::exception {
+ public:
+  NoTaskException() noexcept = default;
+  NoTaskException(NoTaskException const &) noexcept = default;
+  NoTaskException(NoTaskException &&) noexcept = default;
+
+  ~NoTaskException() noexcept = default;
+
+  NoTaskException &operator=(NoTaskException const &) noexcept = default;
+  NoTaskException &operator=(NoTaskException &&) noexcept = default;
+
+  char const *what() const noexcept override;
+
+ private:
+};
+// queue of BigInts, synchronized, with flags to indicate tasks will be added
+class TaskQueue {
+ public:
+  TaskQueue() noexcept;
+  TaskQueue(TaskQueue const &) = delete;
+  TaskQueue(TaskQueue &&) = delete;
+
+  ~TaskQueue() noexcept = default;
+
+  TaskQueue &operator=(TaskQueue const &) = delete;
+  TaskQueue &operator=(TaskQueue &&) = delete;
+
+  // add a task
+  void push(BigInt &&) noexcept;
+  void push(BigInt const &) noexcept;
+
+  // waits for a task, gets it, sets flag for more tasks maybe incoming (from
+  // this pop)
+  // throws NoTaskException if task queue is empty and no more tasks will come
+  BigInt pop();
+
+  // marks no more tasks incoming (from this pop)
+  void done() noexcept;
+
+ private:
+  std::mutex lock;
+  std::condition_variable newTaskFlag;
+  std::queue<BigInt> tasks;
+  size_t maybeMoreCount;  // number of threads that may add tasks
+};
+}  // namespace pandivisible
+
+#endif  // PANDIVISIBLE_QUEUE_H_
